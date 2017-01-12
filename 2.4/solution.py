fe104e1dbb4bab61ab10671f1d96d85d5ce509d9
Sergey Zachepilov <sergey@MacBook-Pro.local>
2017-01-12 23:48:34 +0300

    Добавил решение домашней работы. Урок 2.4

---
+++ 2.4/solution.py
@@ -0,0 +1,30 @@
+# Используйте команду sips
+#
+# Документация:
+# https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/sips.1.html
+#
+# Пример использования для нашей задачи:
+# sips --resampleWidth 200 myphoto.jpg
+
+import os
+import glob
+import subprocess
+import time
+
+
+# Решил замерить время исполнения
+class Profiler(object):
+    def __enter__(self):
+        self._startTime = time.time()
+
+    def __exit__(self, type, value, traceback):
+        print("Elapsed time: {:.3f} sec".format(time.time() - self._startTime))
+
+
+with Profiler() as p:
+    source = 'Source'
+
+    files = glob.glob(os.path.join(source, "*.jpg"))
+
+    for file in files:
+        subprocess.run(['sips', '--resampleWidth', '200', file])
---
+++ 2.4/solution_multy.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import subprocess
+import time
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+# Решил замерить время исполнения
+class Profiler(object):
+    def __enter__(self):
+        self._startTime = time.time()
+
+    def __exit__(self, type, value, traceback):
+        print("Elapsed time: {:.3f} sec".format(time.time() - self._startTime))
+
+
+source = 'Source'
+
+files = glob.glob(os.path.join(source, "*.jpg"))
+
+
+def call_sips(file_name):
+    subprocess.run(['sips', '--resampleWidth', '200', file_name])
+
+
+# Make the Pool of workers
+pool = ThreadPool(4)
+
+with Profiler() as p:
+        pool.map(call_sips, files)
